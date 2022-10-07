f042d1376a1d1f88d7ce95df1eb471d064f07cd9
Hygor Costa <cost0142@algonquinlive.com>
2022-10-06 23:27:47 -0400

    First Commit

---
+++ 300_Player/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  300_Player
+//
+//  Created by Hygor Costa on 2022-10-05.
+//
+
+import Foundation
+
+
+
+var Player2 = PlayerV2(strength: 3, stamina: 3, weapons: 3, money: 3, faction: .bad, name: "Hygor", type: "Warrior", avatar: false, score: 3, health: 3, level: 3, experience: 3 )
+
+
+Player2.output()
+
+var Player5 = PlayerV2()
+
+Player5.output()
+
+
