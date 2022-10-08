e05fc52676e8f237f6ee41cd9f6122ee94db85e0
Hygor Costa <cost0142@algonquinlive.com>
2022-10-08 13:29:37 -0400

    Final Version

--- 300_Player/main.swift
+++ 300_Player/main.swift
@@ -9,13 +9,15 @@ import Foundation
 
 
 
-var Player2 = PlayerV2(strength: 3, stamina: 3, weapons: 3, money: 3, faction: .bad, name: "Hygor", type: "Warrior", avatar: false, score: 3, health: 3, level: 3, experience: 3 )
+var Player1 = PlayerV2(strength: 3, stamina: 3, weapons: 3, money: 3, faction: .bad, name: "Hygor", type: "Warrior", avatar: false, score: 3, health: 3, level: 3, experience: 3 )
+
+Player1.output()
+print("")
 
 
+var Player2 = PlayerV2()
+
+print("Default Parameters")
 Player2.output()
-
-var Player5 = PlayerV2()
-
-Player5.output()
 
 
